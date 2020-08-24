d1bc37aad5aeeb03fcfafe522a866121bf1af376
petros <pit.trak@gmail.com>
2020-08-24 11:45:31 +0300

    Receiver function

--- deck.go
+++ deck.go
@@ -6,6 +6,9 @@ import "fmt"
 // which is slice of strings
 type deck []string // this new type called deck kind of extends or borrows all the beheavor of slices
 
+//   receiver
+//Any variable of type deck now gets access to the print method
+//d is the reference to the actual copy of the deck-type variable. In this case cards
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
