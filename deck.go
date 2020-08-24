cfc9ebefd331fc8c9053579470ef00d266aed47a
petros <pit.trak@gmail.com>
2020-08-24 12:05:33 +0300

    Creating a new deck

--- deck.go
+++ deck.go
@@ -6,6 +6,22 @@ import "fmt"
 // which is slice of strings
 type deck []string // this new type called deck kind of extends or borrows all the beheavor of slices
 
+func newDeck() deck {
+	cards := deck{}
+
+	//slice of strings
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 //   receiver
 //Any variable of type deck now gets access to the print method
 //d is the reference to the actual copy of the deck-type variable. In this case cards
--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Spades", newCard()}
-	cards = append(cards, "Ace of Hearts") // append doesn't modify the current slice, instead it returns a new one
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
