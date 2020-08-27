13d36104f4b4a6e4bbf8bb78e2b767cf7a025b40
petros <pit.trak@gmail.com>
2020-08-27 18:32:39 +0300

    Shuffling a deck

--- deck.go
+++ deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -57,4 +58,13 @@ func newDeckFromFile(filename string) deck {
 	}
 	s := strings.Split(string(bs), ", ")
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+
+		// Swaps i and newPosition with newPosition and i
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
