a652953e25573b3c063a15949a5d887ed2dd7440
petros <pit.trak@gmail.com>
2020-10-07 00:51:46 +0300

    Structs with receiver functions

--- structs/main.go
+++ structs/main.go
@@ -2,18 +2,34 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zip   int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	// // Ways to declare structs
-	// petros := person{firstName: "Petros", lastName: "Trakadas"}
-	// fmt.Println(petros)
-	var me person
-	me.firstName = "Petros"
-	me.lastName = "Trakadas"
-	fmt.Println(me)
-	fmt.Printf("%+v", me)
+	// Ways to declare structs
+	// info := contactInfo{email: "pit.trak@gmail.com", zip: 17456}
+	petros := person{firstName: "Petros", lastName: "Trakadas", contactInfo: contactInfo{email: "pit.trak@gmail.com", zip: 17456}}
+	petros.updateName("Pit")
+	petros.print()
+	// var me person
+	// me.firstName = "Petros"
+	// me.lastName = "Trakadas"
+	// fmt.Println(me)
+	// fmt.Printf("%+v", me)
+}
+
+func (p person) updateName(newName string) {
+	p.firstName = newName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
