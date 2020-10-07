6e7f286fddbd49b5f15440319d65e91edca4e003
petros <pit.trak@gmail.com>
2020-10-07 16:25:35 +0300

    Pass by value - pointer operations

--- structs/main.go
+++ structs/main.go
@@ -17,7 +17,8 @@ func main() {
 	// Ways to declare structs
 	// info := contactInfo{email: "pit.trak@gmail.com", zip: 17456}
 	petros := person{firstName: "Petros", lastName: "Trakadas", contactInfo: contactInfo{email: "pit.trak@gmail.com", zip: 17456}}
-	petros.updateName("Pit")
+	petrosPointer := &petros
+	petrosPointer.updateName("Pit")
 	petros.print()
 	// var me person
 	// me.firstName = "Petros"
@@ -26,8 +27,8 @@ func main() {
 	// fmt.Printf("%+v", me)
 }
 
-func (p person) updateName(newName string) {
-	p.firstName = newName
+func (pointerToPerson *person) updateName(newName string) {
+	(*pointerToPerson).firstName = newName
 }
 
 func (p person) print() {
