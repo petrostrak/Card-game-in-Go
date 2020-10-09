d0b8be2ccf0072ba10ae6d9d3c7a1aed3355005b
petros <pit.trak@gmail.com>
2020-10-09 12:07:53 +0300

    Alternative loop syntax

--- channels/main.go
+++ channels/main.go
@@ -20,8 +20,8 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for l := range c {
+		go checkLink(l, c)
 	}
 }
 
@@ -29,10 +29,10 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
-		c <- "site is down"
+		c <- link
 		return
 	}
 
-	fmt.Println(link)
-	c <- "site is up!"
+	fmt.Println(link, "is up")
+	c <- link
 }
